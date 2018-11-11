b03e1806ed9edcce8e31d9be880c612037574bc5
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-11-10 21:33:01 -0700

    added helpful functions for Skyscanner.Response

---
+++ env.go
@@ -0,0 +1,9 @@
+package main
+
+// DestinationAirports is an array of all airports that should be queried
+var DestinationAirports = [...]string{
+	"CDG-sky",
+	"BNA-sky",
+	"LHR-sky",
+	"ZAG-sky",
+}
